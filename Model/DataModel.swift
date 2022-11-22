5699b65b9d346dda0feb7e83bdbf39b4268b854c
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-22 15:16:03 +0530

    create datamodel file - define id, title and completed value

--- Model/DataModel.swift
+++ Model/DataModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+struct DataModel: Identifiable, Codable {
+    let id: String
+    let title: String
+    let isCompleted: Bool
+    
+    init(id: String = UUID().uuidString ,title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    
+    func updateCompletion() -> DataModel {
+        return DataModel(title: self.title, isCompleted: !self.isCompleted)
+    }
+}
