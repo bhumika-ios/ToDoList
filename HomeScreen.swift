69e1ab6309f675286cb5e297458ac71af6205094
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-22 15:02:47 +0530

    added navigation link for add button - create add animation function set IN and OUT destination timing for effect animation

--- HomeScreen.swift
+++ HomeScreen.swift
@@ -19,16 +19,40 @@ struct HomeScreen: View {
                 Text("Are you a productive person? I think you should click the add button and add a bunch of items to your todo list!")
                     .font(.system(.headline))
                     .padding(.bottom, 20)
-                
-                Text("Add Something 🥳")
-                    .font(.system(.headline))
-                    .foregroundColor(.white)
-                    .frame(height: 55)
-                    .frame(maxWidth: .infinity)
-                    .background(animate ? secondaryAccentColor : Color.accentColor)
-                    .cornerRadius(10)
+                NavigationLink(destination:){
+                    Text("Add Something 🥳")
+                        .font(.system(.headline))
+                        .foregroundColor(.white)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(animate ? secondaryAccentColor : Color("Green"))
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal, animate ? 30 : 50)
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -10 : .zero)
+                .shadow(color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                        radius: animate ? 30 : 10,
+                        x: 0,
+                        y: animate ? 50 : 30)
             }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
         }
+    }
+    //MARK: - Methods
+    private func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                self.animate.toggle()
+            }
+        })
     }
 }
 
