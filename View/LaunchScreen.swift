accaa2639eb6068703f83f59ff0aefdc48558de0
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-22 16:42:42 +0530

    side curv set on launchscreen- create class liquideswipe and call in clip shape

--- View/LaunchScreen.swift
+++ View/LaunchScreen.swift
@@ -12,7 +12,9 @@ struct LaunchScreen: View {
         
         ZStack{
             Color("Green")
+                .clipShape(LiquidSwipe())
                 .edgesIgnoringSafeArea(.all)
+            
             VStack{
                 Image(systemName: "checkmark")
                     .resizable()
@@ -37,8 +39,26 @@ struct LaunchScreen_Previews: PreviewProvider {
         LaunchScreen()
     }
 }
-//struct LiquidSwipe: Shape {
-//    func path(in rect: CGRect) -> Path{
-//        return Path{}
-//    }
-//}
+struct LiquidSwipe: Shape {
+    func path(in rect: CGRect) -> Path{
+        return Path{path in
+            let width = rect.width
+            //first constructing rectangle shape..
+            
+            path.move(to: CGPoint(x: 0, y: 0))
+            path.addLine(to: CGPoint(x: rect.width, y: 0))
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+            
+            //now constructing curve shape...
+            
+                //from
+            
+            path.move(to: CGPoint(x: width, y: 80))
+            
+            //mid betwween 80-180..
+            let mid : CGFloat = 80 + ((180 - 80) / 2)
+            path.addCurve(to: CGPoint(x: width, y: 180), control1: CGPoint(x: width - 50, y: mid), control2: CGPoint(x: width - 50, y: mid))
+        }
+    }
+}
