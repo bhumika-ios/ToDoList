22ed844282f9e328c43a1d2f513234bf5b7c52ca
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-22 15:35:56 +0530

    create row value and used datamodel value

--- View/ListRowView.swift
+++ View/ListRowView.swift
@@ -22,5 +22,22 @@ struct ListRowView: View{
             
             Spacer()
         }
+        .font(.title)
+        .padding(.vertical, 8)
     }
+}
+
+    //MARK: - Preview
+struct ListRowView_Previews: PreviewProvider{
+    static var data1 = DataModel(title: "First Data!", isCompleted: false)
+    static var data2 = DataModel(title: "Second Data!", isCompleted: true)
+    static var previews: some View {
+        Group {
+            ListRowView(data: data1)
+            ListRowView(data: data2)
+        }
+        .previewLayout(.sizeThatFits)
+    }
+    
+    
 }
